9dd3cec2d255e5c2f301ff4b6e5e02d6dcdf985e
Guillermo Mira Osuna <miraosuna@usal.es>
2022-05-04 10:15:09 +0200

    1

--- modulo02/ex00/Fixed.cpp
+++ modulo02/ex00/Fixed.cpp
@@ -6,8 +6,73 @@
 /*   By: guilmira <guilmira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 14:15:22 by guilmira          #+#    #+#             */
-/*   Updated: 2022/05/03 14:40:41 by guilmira         ###   ########.fr       */
+/*   Updated: 2022/05/04 10:05:59 by guilmira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+
+#define DECIMAL_BIT_NBR 8
+
+const int Fixed::_enteroConst = DECIMAL_BIT_NBR;
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called." << std::endl;
+	return ;
+}
+
+Fixed::Fixed(Fixed const &fixed1)
+{
+	std::cout << "Copy constructor called." << std::endl;
+	std::cout << "Class origin and source are actually the same. Indistinguishable." << std::endl;
+	*this = fixed1;
+	return ;
+}
+
+Fixed::~Fixed()
+{
+	return ;
+}
+
+
+Fixed & Fixed::operator=(Fixed const &rhs)
+{
+	if (this != &rhs)
+	{
+		//igualar todas las privadas.
+		return ;
+	}
+		
+	return (*this);
+}
+
+Example::Example()
+{
+	this->x = 5;
+	return ;
+}
+
+Example::Example(Example const &src)
+{
+	std::cout << "Copy constructor called." << std::endl;
+	*this = src;
+	return ;
+}
+
+//basicamente, =, es cambiar el puntero this. (POR SUPUESTO, no cte.)
+Example & Example::operator=(Example const &rhs)
+{
+	std::cout << "ASSIGNMENT called." << std::endl;
+
+	if (this != &rhs)
+		this->x = rhs.x;
+	
+	return (*this);
+	
+}
+
+Example::~Example()
+{
+	return ;
+}
--- modulo02/ex00/Fixed.hpp
+++ modulo02/ex00/Fixed.hpp
@@ -6,11 +6,41 @@
 /*   By: guilmira <guilmira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 14:15:28 by guilmira          #+#    #+#             */
-/*   Updated: 2022/05/03 14:41:12 by guilmira         ###   ########.fr       */
+/*   Updated: 2022/05/04 10:11:03 by guilmira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
 class Fixed
 {
-	
+	public:
+		Fixed();
+		Fixed(Fixed const &fixed1);
+		
+
+		~Fixed();
+
+		Fixed & operator=(Fixed const &rhs);
+
+	private:
+		int _entero;
+		const static int _decimalBits;
+		int _decimal;
+		int _width;
+		int _bincomma;
+};
+
+class Example
+{
+	public:
+		Example();
+		Example(Example const &src);
+		Example & operator=(Example const &rhs);
+
+		
+		~Example();
+	private:
+		int x;
+
 };
