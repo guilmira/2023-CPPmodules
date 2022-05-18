5699ed565e54e48b6fe491e15ea63dede3904456
Guillermo Mira Osuna <miraosuna@usal.es>
2022-05-18 14:03:06 +0200

    2

--- modulo02/ex02/Fixed.cpp
+++ modulo02/ex02/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: guilmira <guilmira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 14:15:22 by guilmira          #+#    #+#             */
-/*   Updated: 2022/05/17 20:07:09 by guilmira         ###   ########.fr       */
+/*   Updated: 2022/05/18 14:00:43 by guilmira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,17 @@
 
 const int Fixed::_decimalBits = DECIMAL_BIT_NBR;
 
+/* --------------------------------- LOG and coutOVERLOAD --------------------------------- */
 static void log(std::string const &str)
 {
 	std::cout << str << std::endl;
 }
 
+std::ostream & operator<<(std::ostream &stream, Fixed const &fixedClass)
+{
+	std::cout << fixedClass.toFloat();
+	return (stream);
+}
 /* --------------------------------- INT CONST --------------------------------- */
 static int parser(const int i)
 {
@@ -32,6 +38,8 @@ static int parser(const int i)
 
 //32 bits, de los cuales marcamos 8 como decimales. 	00000000 00000000 00000000 . 00000000
 //al guardarse en un int, es siempre un fixed(32,8).  	value is initialized to avoid seg faults before parser
+//formula could also be:
+//this->_value = i * (1 << Fixed::_decimalBits)
 Fixed::Fixed(const int i)
 	: _value(0)
 {
@@ -48,6 +56,7 @@ Fixed::Fixed(const int i)
 //1 << scale	=	0001  0000 0000		= 256
 //2^8			=	0001  0000 0000		= 256
 //therefore 		2^scale = 1<<scale
+//makes sense since we know that apply number<<1, is equivalent to multiplying times 2. moving 2 spaces, 2*2 // moving 3 spaces: 2*2*2  = 2^3
 //2.5 * 1<<scale=   0010  1000 0000		= 640   Tenemos el FBP perfecto, ya que ese bit en la octava posicion, representa 2^-1 = 0.5
 static int float_to_FBP(const float fl, int decimal_bits)
 {
@@ -104,7 +113,7 @@ Fixed::Fixed(Fixed const &src)
 	return ;
 }
 
-/* --------------------------------- OVERLOAD OUTPUT-STREAM --------------------------------- */
+/* --------------------------------- TO INT AND TO FLOAT --------------------------------- */
 int		Fixed::toInt(void) const
 {
 	int	decimal_int;
@@ -127,12 +136,6 @@ float	Fixed::toFloat(void) const
 	binary_fixed_nbr = ( (float) this->getRawBits() );
 	float_representation = binary_fixed_nbr / ((float) (1 << Fixed::_decimalBits));
 	return (float_representation);
-}
-
-std::ostream & operator<<(std::ostream &stream, Fixed const &fixedClass)
-{
-	std::cout << fixedClass.toFloat();
-	return (stream);
 }
 
 /* --------------------------------- OPERATOR OVERLOAD --------------------------------- */
@@ -177,11 +180,13 @@ int Fixed::operator!=(Fixed const &rhs) const
 	return ((this->getRawBits() != rhs.getRawBits()));
 }
 
+/* --------------------------------- Basic algebra --------------------------------- */
 Fixed Fixed::operator+(Fixed const &rhs)
 {
 	Fixed object_sum;
 
-	object_sum.setRawBits(this->getRawBits() + rhs.getRawBits());
+	object_sum.setRawBits\
+	(this->getRawBits() + rhs.getRawBits());
 	return (object_sum);
 }
 
@@ -189,7 +194,8 @@ Fixed Fixed::operator-(Fixed const &rhs)
 {
 	Fixed result;
 
-	result.setRawBits(this->getRawBits() - rhs.getRawBits()); 
+	result.setRawBits\
+	(this->getRawBits() - rhs.getRawBits()); 
 	return (result);
 }
 
@@ -217,8 +223,24 @@ Fixed Fixed::operator/(Fixed const &rhs)
 	return (result);
 }
 
-/* --------------------------------- Incrementation --------------------------------- */
+/* --------------------------------- Incrementation/decrementation --------------------------------- */
 
+//SOURCE -STACKOVERFLOW
+/* No, you don't have to return the reference to your object when you overload the pre-increment operator. In fact you may return anything you'd like, MyClass, int, void, whatever.
+
+This is a design issue -- you must ask yourself what is the most useful thing to the users of your class that you are able to return.
+
+As a general rule, class operators are the most useful when they cause the least confusion, that is, when they operate the most like operators on basic types. In this case, the pre-increment operator on a basic type:
+
+int  i = 7;
+j = ++i;
+increments the variable and then returns the new value. If this is the only use you want MyClass to have, then returning a copy of your class is sufficient.
+
+But, the pre-increment operator on a basic type actually returns an lvalue. So, this is legal:
+
+int i = 7;
+int *p = &++i;
+If you want to support an operation like this, you must return a reference. */
 Fixed & Fixed::operator++()
 {
 	this->setRawBits(this->getRawBits() + 1);
@@ -226,6 +248,7 @@ Fixed & Fixed::operator++()
 }
 
 //exactly the same as ++i, but the return has to be a COPY of the previous state.
+//it cant be a reference to the actual state for obvious reasons.
 Fixed Fixed::operator++(int)
 {
 	Fixed tmp(*this); //copy constructor used to replicate a temporal class.
