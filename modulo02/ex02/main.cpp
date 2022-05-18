cf8d726574bb3f561d32e0daae2d0b801075aefd
Guillermo Mira Osuna <miraosuna@usal.es>
2022-05-18 15:57:04 +0200

    2

--- modulo02/ex02/main.cpp
+++ modulo02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: guilmira <guilmira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 14:12:31 by guilmira          #+#    #+#             */
-/*   Updated: 2022/05/18 13:23:50 by guilmira         ###   ########.fr       */
+/*   Updated: 2022/05/18 15:56:31 by guilmira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,15 @@ int main(void)
 {
 	Fixed		a(0);
 	Fixed const	b(Fixed(5.05f) * Fixed(2));
-	Fixed const	c(Fixed(6) / Fixed(3));
 
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
 	std::cout << a << std::endl;
 	std::cout << a++ << std::endl;
 	std::cout << a << std::endl;
-	std::cout << "multipication" << std::endl;
+
 	std::cout << b << std::endl;
-	std::cout << c << std::endl;
+	
 	std::cout << Fixed::max(a,b) << std::endl;
 	return (0);
 }
