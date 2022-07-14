0f2e4adccbf9da5c137f13737a964ac793969bb6
Guillermo Mira Osuna <miraosuna@usal.es>
2022-07-14 12:27:52 +0200

    2

--- modulo03/ex03/main.cpp
+++ modulo03/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: guilmira <guilmira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/15 22:04:27 by guilmira          #+#    #+#             */
-/*   Updated: 2022/05/16 14:13:07 by guilmira         ###   ########.fr       */
+/*   Updated: 2022/07/14 12:19:03 by guilmira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include "FragTrap.hpp"
 #include "DiamondTrap.hpp"
 
-
+aqui estoy revisando
 //PENDIENTE
 //ademas falta pedir ayuda de las flags raras de compilacion
 int	main(void)
